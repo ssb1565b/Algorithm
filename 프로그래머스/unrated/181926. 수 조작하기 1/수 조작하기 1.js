0d4ecdbc7a6bf65a5a87ae20856238c9c7c03364
Song Su Bin / 송수빈 <84695884+ssb1565b@users.noreply.github.com>
2023-04-30 14:25:10 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.94 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,12 @@
+const solution = (n, control) =>
+  [...control]
+    .map((el) => {
+      return el === "w"
+        ? (n += 1)
+        : el === "s"
+        ? (n -= 1)
+        : el === "d"
+        ? (n += 10)
+        : (n -= 10);
+    })
+    .slice(-1)[0];
