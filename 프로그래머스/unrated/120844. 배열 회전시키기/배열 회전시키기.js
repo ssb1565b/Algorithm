87814be67b9ee055d65ddcff1cac2429e5bfbfc2
Song Su Bin / 송수빈 <84695884+ssb1565b@users.noreply.github.com>
2023-10-26 01:57:41 +0900

    [level unrated] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,9 @@
+function solution(numbers, direction) {
+    if (direction === 'right') {
+       return  [numbers[numbers.length-1],...numbers].slice(0,numbers.length)
+    }
+    if (direction === 'left') {
+        return [...numbers] = [...numbers,numbers[0]].slice(1)
+        
+    }
+}
